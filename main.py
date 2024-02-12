482906be995cc54926308710bc305017f0082bdc
AppSolves <appsolves@gmail.com>
2024-02-12 18:05:13 +0100

    Refactor Bluetooth connection handling and add cleaning status updates

--- main.py
+++ main.py
@@ -3,28 +3,35 @@
 # It is subject to the terms and conditions of the CC BY-NC-ND 4.0 license.
 
 
+import asyncio
+
 from src.cleaning_robot import (
     CleaningRobot,
 )  # Importing the CleaningRobot class and time module
-from src.connections import asyncio, setup_bluetooth
+from src.connections import ConnectionManager
+
+connection_manager = (
+    ConnectionManager()
+)  # Creating a new instance of the ConnectionManager class
+robot = CleaningRobot()  # Creating a new instance of the CleaningRobot class
 
 
 async def main():
-    robot = CleaningRobot()  # Creating a new instance of the CleaningRobot class
-
     while True:
         if (
             robot.startstop_button.is_pressed and not robot.is_cleaning
         ):  # If the start/stop button is pressed and the robot is not cleaning
             robot.start_routine()  # Start the cleaning routine
+            await connection_manager.write({"is_cleaning": robot.is_cleaning})
         elif robot.startstop_button.is_pressed and robot.is_cleaning:
             robot.stop_routine()
+            await connection_manager.write({"is_cleaning": robot.is_cleaning})
 
         await asyncio.sleep(0.01)  # Sleep for 0.01 seconds
 
 
-# Run the main function and the imported setup_bluetooth function in parallel:
+# Run the main function and the connection manager in parallel
 loop = asyncio.get_event_loop()
 loop.create_task(main())
-loop.create_task(setup_bluetooth())
+loop.create_task(connection_manager.initialize())
 loop.run_forever()
--- src/connections.py
+++ src/connections.py
@@ -10,81 +10,87 @@ import bluetooth  # type:ignore
 from micropython import const  # type:ignore
 
 from src.cleaning_robot import CleaningRobot
+from src.config import Singleton
 
 
-async def setup_bluetooth():
-    _MICROCONTROLLER_SERVICE_UUID = bluetooth.UUID(
-        "57b83ac1-34d0-418a-bf25-bfacd5d9ac3a"
-    )
-    _MICROCONTROLLER_DATA_CHAR_UUID = bluetooth.UUID(
-        "57b83ac2-34d0-418a-bf25-bfacd5d9ac3a"
-    )
-    _APPEARANCE = const(384)  # type: ignore
-    _ADV_INTERVAL_US = const(250000)  # type: ignore
+@Singleton
+class ConnectionManager:
+    def __init__(self) -> None:
+        self.__MICROCONTROLLER_SERVICE_UUID__ = bluetooth.UUID(
+            "57b83ac1-34d0-418a-bf25-bfacd5d9ac3a"
+        )
+        self.__MICROCONTROLLER_DATA_CHAR_UUID__ = bluetooth.UUID(
+            "57b83ac2-34d0-418a-bf25-bfacd5d9ac3a"
+        )
+        self.__APPEARANCE__ = const(384)  # type: ignore
+        self.__ADV_INTERVAL_US__ = const(250000)  # type: ignore
 
-    microcontroller_service = aioble.Service(_MICROCONTROLLER_SERVICE_UUID)
-    data_char = aioble.Characteristic(
-        microcontroller_service,
-        _MICROCONTROLLER_DATA_CHAR_UUID,
-        read=True,
-        write=True,
-        notify=True,
-        capture=True,
-    )
+        microcontroller_service = aioble.Service(self.__MICROCONTROLLER_SERVICE_UUID__)
+        self.__data_char__ = aioble.Characteristic(
+            microcontroller_service,
+            self.__MICROCONTROLLER_DATA_CHAR_UUID__,
+            read=True,
+            write=True,
+            notify=True,
+            capture=True,
+        )
 
-    aioble.register_services(microcontroller_service)
-    aioble.core.ble.gatts_set_buffer(data_char._value_handle, 512)
+        aioble.register_services(microcontroller_service)
+        aioble.core.ble.gatts_set_buffer(self.__data_char__._value_handle, 512)
 
-    robot = CleaningRobot()
+        self.__robot__ = CleaningRobot()
 
-    async def __write__(data: dict):
-        return await data_char.write(
+    async def initialize(self):
+        await asyncio.gather(
+            self.__wait_connections__(),
+            self.__listener__(),
+        )
+
+    async def write(self, data: dict):
+        return await self.__data_char__.write(
             json.dumps(data).encode("utf-8"),
             send_update=True,
         )
 
-    async def __handle_commands__(data: str):
+    async def __handle_commands__(self, data: str):
+        async def __send_cleaning_status__():
+            is_cleaning_data = {"is_cleaning": self.__robot__.is_cleaning}
+            await self.write(is_cleaning_data)
+
         try:
             command = json.loads(data)
             if command["command"] == "request_cleaning_status":
-                is_cleaning_data = {"is_cleaning": robot.is_cleaning}
-                await __write__(is_cleaning_data)
+                await __send_cleaning_status__()
             elif command["command"] == "start_cleaning":
-                robot.start_routine()
-                if robot.is_cleaning:
-                    is_cleaning_data = {"is_cleaning": robot.is_cleaning}
-                    await __write__(is_cleaning_data)
+                self.__robot__.start_routine()
+                await __send_cleaning_status__()
             elif command["command"] == "stop_cleaning":
-                robot.stop_routine()
-                if not robot.is_cleaning:
-                    is_cleaning_data = {"is_cleaning": robot.is_cleaning}
-                    await __write__(is_cleaning_data)
+                self.__robot__.stop_routine()
+                await __send_cleaning_status__()
         except Exception as e:
             if type(e) is not TypeError:
                 print(f"Error Handling Command: {e}")
 
-    async def __wait_connections__():
+    async def __wait_connections__(self):
         while True:
             try:
                 async with await aioble.advertise(
-                    _ADV_INTERVAL_US,
+                    self.__ADV_INTERVAL_US__,
                     name="SmartSweep GT",
-                    services=[_MICROCONTROLLER_SERVICE_UUID],
-                    appearance=_APPEARANCE,
+                    services=[self.__MICROCONTROLLER_SERVICE_UUID__],
+                    appearance=self.__APPEARANCE__,
                 ) as connection:
                     await connection.disconnected(timeout_ms=None)
             except Exception as e:
                 print(f"Error Connection Waiter: {e}")
 
-    async def __listener__():
+    async def __listener__(self):
         while True:
             try:
-                _, data = await data_char.written()
+                _, data = await self.__data_char__.written()
                 res = data.decode("utf-8")
-                await __handle_commands__(res)
+                await self.__handle_commands__(res)
                 await asyncio.sleep(0.1)
             except Exception as e:
                 print(f"Error Listener: {e}")
                 await asyncio.sleep(1)
-
-    await asyncio.gather(__wait_connections__(), __listener__())
